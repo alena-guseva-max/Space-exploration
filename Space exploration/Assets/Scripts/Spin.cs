2a533ec93177039933f25f8cbaaf37e8c8f07d56
alena-guseva-max <guseva2201@gmail.com>
2022-02-25 14:21:10 +0300

    First commit

---
+++ Space exploration/Assets/Scripts/Spin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 3.0f; //Объявление общедоступной переменной для скорости вращения
+
+    void Update()
+    {
+        transform.Rotate(0, speed, 0); //команда Rotate(вращение) будет запускаться в каждом кадре.
+    }
+}
